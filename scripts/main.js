91d6611350f96a004de3f7bf4ebea838089a594a
Петър Илиев <petyr2203@abv.bg>
2026-06-09 14:51:40 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import "./api.js";
 import "./BrandInfinityScroll/brandInfinityScroll.js";
 import "./HamburgerBtn/hamburgerAnimation.js";
